994b1226c28d54e5a3a4a81716ded1074a9cc712
wangkaiyan <kaiyanky2@gmail.com>
2019-07-21 22:15:05 +0800

    Initial commit

---
+++ app/src/main/java/pri/ky2/ky2coderepos/MainActivity.java
@@ -0,0 +1,13 @@
+package pri.ky2.ky2coderepos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
