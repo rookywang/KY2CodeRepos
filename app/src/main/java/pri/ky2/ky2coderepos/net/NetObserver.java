0e49e5aaf6ce894b6ced3a4388b3631b44e843a5
wangkaiyan <kaiyanky2@gmail.com>
2019-07-25 21:02:27 +0800

    在 NetObserver 中增加网络请求的方法

--- app/src/main/java/pri/ky2/ky2coderepos/net/NetObserver.java
+++ app/src/main/java/pri/ky2/ky2coderepos/net/NetObserver.java
@@ -5,7 +5,11 @@ import android.util.Log;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.blankj.utilcode.util.ActivityUtils;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import io.reactivex.SingleObserver;
@@ -13,6 +17,7 @@ import io.reactivex.disposables.Disposable;
 import pri.ky2.ky2coderepos.interfaces.ILoadingView;
 import pri.ky2.ky2coderepos.utils.ErrorToMsgUtils;
 import pri.ky2.ky2coderepos.utils.GsonUtils;
+import pri.ky2.ky2coderepos.utils.ToastUtils;
 
 /**
  * 网络请求观察者
@@ -30,7 +35,7 @@ public abstract class NetObserver<T> implements SingleObserver<NetResponse> {
 
     /**
      * 网络请求成功响应码
-     * TODO：得改
+     * TODO：网络请求成功响应码得改
      */
     private static final String NET_SUCCESS = "123456";
 
@@ -53,6 +58,16 @@ public abstract class NetObserver<T> implements SingleObserver<NetResponse> {
      * 字段（返回数据 data 部分只有单个字段）的名称
      */
     private String mFieldKey;
+
+    /**
+     * Header 键值对
+     */
+    private HashMap<String, String> mHeaders;
+
+    /**
+     * Body 键值对
+     */
+    private HashMap<String, String> mParams;
 
     public NetObserver(Class<T> cls, ILoadingView iLoadingView) {
         mTClass = cls;
@@ -100,17 +115,28 @@ public abstract class NetObserver<T> implements SingleObserver<NetResponse> {
     public void onOriginDataSuccess(String response, String data) {
     }
 
-    public abstract void onError(String code, String msg);
-
     /**
-     * 当返回数据 data 中只有一个字段时设置字段名称
+     * 请求失败
      *
-     * @param key 要解析的字段
-     * @return
+     * @param code 错误码
+     * @param msg  错误信息
      */
-    public NetObserver setKey(String key) {
-        mFieldKey = key;
-        return this;
+    public void onError(String code, String msg) {
+        Log.i(TAG, "onError code: " + code + "msg: " + msg);
+        if (mILoadingView != null && mILoadingView.isClosed()) {
+            return;
+        }
+        if (mILoadingView != null) {
+            try {
+                String topName = ActivityUtils.getTopActivity().getClass().getSimpleName();
+                if (!TextUtils.isEmpty(topName) && !topName.equals(mILoadingView.getUIName())) {
+                    return;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        ToastUtils.show(msg);
     }
 
     @Override
@@ -192,6 +218,7 @@ public abstract class NetObserver<T> implements SingleObserver<NetResponse> {
             }
             String responseMsg = netResponse.getRlt_msg();
             String errMsg = TextUtils.isEmpty(responseMsg) ? "服务器异常" + responseCode : responseMsg;
+            //TODO：这里可以做一个登录失效的判断
             onError(responseCode, errMsg);
             return true;
         }
@@ -213,4 +240,110 @@ public abstract class NetObserver<T> implements SingleObserver<NetResponse> {
             mILoadingView.hideLoadingDialog();
         }
     }
+
+    /**
+     * 增加 Header
+     */
+    public NetObserver<T> addHeader(String key, String value) {
+        if (mHeaders == null) {
+            mHeaders = new HashMap<>();
+        }
+        if (!TextUtils.isEmpty(key) && !TextUtils.isEmpty(value)) {
+            mHeaders.put(key, value);
+        }
+        return this;
+    }
+
+    /**
+     * 增加参数
+     */
+    public NetObserver<T> addParam(String key, String value) {
+        if (mParams == null) {
+            mParams = new HashMap<>();
+        }
+        if (!TextUtils.isEmpty(key) && !TextUtils.isEmpty(value)) {
+            mParams.put(key, value);
+        }
+        return this;
+    }
+
+    public NetObserver<T> addParam(String key, int value) {
+        addParam(key, String.valueOf(value));
+        return this;
+    }
+
+    public void setParamMap(HashMap<String, String> map) {
+        mParams = map;
+    }
+
+    /**
+     * 当返回数据 data 中只有一个字段时设置字段名称
+     *
+     * @param key 要解析的字段
+     * @return
+     */
+    public NetObserver setKey(String key) {
+        mFieldKey = key;
+        return this;
+    }
+
+    /**
+     * GET 方法
+     */
+    public void get(String url) {
+        NetRequest.doGet(url, this);
+    }
+
+    /**
+     * POST 方法
+     */
+    public void post(String url) {
+        addToken();
+        NetRequest.doPost(url, mHeaders, mParams, this);
+    }
+
+    /**
+     * POST 方法-Json
+     *
+     * @param url
+     */
+    public void postJson(String url) {
+        addToken();
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(mParams));
+        NetRequest.doPostJson(url, mHeaders, jsonObject.toJSONString(), this);
+    }
+
+    /**
+     * POST 方法-上传多文件
+     */
+    public void uploadMultiFiles(String url, List<File> files) {
+        addToken();
+        NetRequest.doUploadMultiFiles(url, mParams, files, this);
+    }
+
+    /**
+     * 上传单文件
+     */
+    public void upload(String url, File file) {
+        List<File> files = new ArrayList<>();
+        files.add(file);
+        uploadMultiFiles(url, files);
+    }
+
+    /**
+     * 将 token 增加到请求参数里面
+     */
+    private void addToken() {
+        if (null == mParams) {
+            mParams = new HashMap<>(10);
+        }
+        if (!mParams.containsKey("access_token")) {
+            String token = "";
+            // TODO：这里要从数据库获取 Token
+            if (!TextUtils.isEmpty(token)) {
+                mParams.put("access_token", token);
+            }
+        }
+    }
+
 }
